e48d164909e7e26b1f35dc1bc20dce3b6ee1c92b
zuhaibahmed444 <zuhaibahmed444@gmail.com>
2021-07-19 20:31:19 +0530

    first

---
+++ config/db.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const mongoose = require("mongoose");
+try {
+  mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+  console.log("Database Connected Successfully");
+} catch (err) {
+  console.log("Database Not Connected");
+}
